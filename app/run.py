a1443d9a67bf048939da63f3362e3f4123298c3c
flyersworder <yeqi519@gmail.com>
2022-09-17 20:16:18 +0200

    add two figures

--- app/run.py
+++ app/run.py
@@ -1,9 +1,12 @@
+import re
 import json
 import plotly
 import pandas as pd
+import swifter
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 from flask import Flask
 from flask import render_template, request, jsonify
@@ -16,7 +19,19 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 def tokenize(text):
+    '''
+    Tokenize the input text.
+
+            Parameters:
+                    text (str): Input text
+
+            Returns:
+                    clean_tokens (list): A list of clean tokens
+    '''      
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text) 
+    
     tokens = word_tokenize(text)
+    tokens = [w for w in tokens if w.lower() not in stopwords.words("english")]
     lemmatizer = WordNetLemmatizer()
 
     clean_tokens = []
@@ -44,12 +59,25 @@ def index():
     genre_counts = pd.DataFrame(df.groupby('genre').count()['message']).reset_index()
     genre_counts.rename(columns={'message':'count'}, inplace=True)
     
-    fig = px.bar(genre_counts, x="genre", y="count", title='Distribution of Message Genres')
+    fig_genre = px.bar(genre_counts, x="genre", y="count", title='Distribution of Message Genres')
 
-    
+    # word frequency
+    words = df['message'].swifter.apply(lambda x: tokenize(x))
+    freq = pd.DataFrame(words.astype(str).str[1:-1].str.split(',', expand=True).stack(
+        ).value_counts()).reset_index().head(20)
+    freq.columns = ['word', 'count']
+
+    fig_freq = px.bar(freq, x='word', y='count', title='Top 20 most frequent words')
+
+    # category frequency
+    categories = pd.DataFrame(df[df.columns[4:]].sum().sort_values()).reset_index()
+    categories.columns = ['category', 'count']
+
+    fig_cat = px.bar(categories, x='count', y='category', orientation='h', 
+        width=1000, height=800, title='Category appearance (=1) frequency')
 
     # create visuals
-    graphs = [fig.to_dict()]
+    graphs = [fig_genre.to_dict(), fig_freq.to_dict(), fig_cat.to_dict()]
     
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
